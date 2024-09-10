390ec9d1607276603de43891f6e78642cb786704
Odnostorontseva Evgenia <e.odnostorontseva@g.nsu.ru>
2024-09-10 14:07:17 +0700

    correcting mistakes

--- Task_1_1_1/src/main/java/nsu/odnostorontseva/HeapSort.java
+++ Task_1_1_1/src/main/java/nsu/odnostorontseva/HeapSort.java
@@ -75,6 +75,5 @@ public class HeapSort {
      *
      * @param args (базовый параметр)
      */
-    public static void main(String[] args) {
-    }
+    public static void main(String[] args) {}
 }
