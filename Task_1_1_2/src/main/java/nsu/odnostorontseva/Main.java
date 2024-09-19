d6dded3f7448c9081dc5d444666231c19b1f867d
Odnostorontseva Evgenia <e.odnostorontseva@g.nsu.ru>
2024-09-19 21:32:42 +0700

    forgot to add main file))

---
+++ Task_1_1_2/src/main/java/nsu/odnostorontseva/Main.java
@@ -0,0 +1,17 @@
+package nsu.odnostorontseva;
+
+/**
+ * start the Game.
+ */
+public class Main {
+    /**
+     * start the BlackJack game.
+     *
+     * @param args (не используются)
+     */
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать в BlackJack!");
+
+        BlackJack blackJack = new BlackJack();
+    }
+}
