5a0da79cb8b26249751d9c8d8f426d57fb19e5f1
zhenechkaaaa <zhenechkaaaa@users.noreply.github.com>
2024-10-08 08:37:56 +0000

    Deploying to gh-pages from @ zhenechkaaaa/OOP@b2a1f4e1be8425d802c81b75a660cd02e6c9a0ab 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.odnostorontseva.task3"},{"l":"ru.nsu.odnostorontseva.task3.operands"},{"l":"ru.nsu.odnostorontseva.task3.operations"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.odnostorontseva.task3"},{"l":"ru.nsu.odnostorontseva.task3.operands"},{"l":"ru.nsu.odnostorontseva.task3.operations"}];updateSearchResults();
