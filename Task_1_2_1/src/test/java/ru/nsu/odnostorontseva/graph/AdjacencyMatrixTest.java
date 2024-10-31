a608992209cff380fe7b5db294a5282917f274c0
Odnostorontseva Evgenia <e.odnostorontseva@g.nsu.ru>
2024-10-31 11:24:47 +0700

    added some changes

--- Task_1_2_1/src/test/java/ru/nsu/odnostorontseva/graph/AdjacencyMatrixTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/odnostorontseva/graph/AdjacencyMatrixTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
